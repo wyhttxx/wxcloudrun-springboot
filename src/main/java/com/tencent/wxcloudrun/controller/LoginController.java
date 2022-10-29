ed5b230be834c6ea22fa803e67878593aa65d4c0
wyhttxx <china.zzh.good@live.cn>
2022-10-29 22:04:32 +0800

    测试获取用户id接口

--- src/main/java/com/tencent/wxcloudrun/controller/LoginController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,11 +15,11 @@ import java.util.Map;
 public class LoginController {
     /**
      * 获取用户openid, 并将session存入redis，发送到前端
-     * @param code
+     * @param codeMap
      * @return
      */
     @RequestMapping(value = "/doLogin", method = RequestMethod.POST)
-    public Map<String, Object> appDoLogin(@RequestBody  Map<String,String > codeMap){
+    public Map<String, Object> appDoLogin(@RequestBody Map<String,Object> codeMap) throws JsonProcessingException {
         System.out.println(codeMap);
         Map<String, Object> map = new HashMap<>();
         if(codeMap.get("code") == null){
@@ -25,7 +27,7 @@ public class LoginController {
             return map;
         }
 
-        String url = new ComonUtils().appendUrl(codeMap.get("code")).toString();
+        String url = new ComonUtils().appendUrl("0739s40w3K3euZ23mh0w3Rsll209s40b").toString();
         RestTemplate restTemplate = new RestTemplate();
 
         //用restTemplate请求url，得到openid和session_key
@@ -37,6 +39,13 @@ public class LoginController {
         }
 
         System.out.println(reponse);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map responseMap = objectMapper.readValue(reponse, Map.class);
+        if(responseMap.get("errcode")=="0000"){
+            map.put("success", false);
+            map.put("errmsg","code 无效");
+            return map;
+        }
         String openid = "";
         String session_key = "";
         String[] strings = reponse.split(",");
@@ -44,8 +53,8 @@ public class LoginController {
         session_key = strings[0].substring(16, strings[0].length()-1);
 
         AppUserModel appUserModel = new AppUserModel();
-        appUserModel.setOpenid(openid);
-        appUserModel.setSession_key(session_key);
+        appUserModel.setOpenid((String) responseMap.get("openid"));
+        appUserModel.setSession_key((String) responseMap.get("session_key"));
 
         //将openid和session_key存入redis,之后传到前端的sessionId就是
         //"userSession:" + appUserModel.getOpenid()
@@ -55,6 +64,7 @@ public class LoginController {
         //System.out.println(openid);
         map.put("sessionId","userSession:" + appUserModel.getOpenid());
         map.put("success", true);
+        map.put("openid","openid:"+appUserModel.getOpenid());
         System.out.println(map);
         return map;
     }
